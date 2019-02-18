aef39f8c7fb152bd7ee516e1532fa2d04b626fe6
James Hooper <jameshooperdesigns@gmail.com>
2019-02-18 14:05:59 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Imports routes for the product
+const product = require('./routes/product.route');
+
+// initialize our express app
+const app = express();
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb+srv://jhooperdev:servingtheweb2019!$@product-cluster-iyok6.gcp.mongodb.net/product';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, {useNewUrlParser: true});
+// Below is deprecated!
+// mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MomgoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/products', product);
+
+let port = 5000;
+
+app.listen(port, () => {
+    console.log('Server is up and running on port number ' + port);
+});
+
---
+++ routes/product.route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// Require the controllers 
+const product_controller = require('../controllers/product.controller');
+
+// A Simple test url to check that all of our files are communicating correctly
+router.get('/test', product_controller.test);
+router.get('/:id', product_controller.product_details);
+router.post('/create', product_controller.product_create);
+
+module.exports = router;
