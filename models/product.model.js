ee4f00bb8db2d5a0a14715496b7c2a6158767260
James Hooper <jameshooperdesigns@gmail.com>
2019-02-20 13:18:40 -0700

    Define the product line

--- models/product.model.js
+++ models/product.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// SpaceX Clothing Product Schema
 let ProductSchema = new Schema({
     name: { type: String, required: true, max: 100 },
     price: { type: Number, required: true},
