b9d43f3245a64b55489cadb462e1406273d8443b
James Hooper <jameshooperdesigns@gmail.com>
2019-02-20 21:17:24 -0700

    Update all defined router endpoints

--- routes/product.route.js
+++ routes/product.route.js
@@ -7,6 +7,7 @@ const product_controller = require('../controllers/product.controller');
 // A simple test url to check all files are communicating correctly
 router.get('/test', product_controller.test);
 
+// Define router for all 6 endpoints
 router.get('/:id', product_controller.product_details);
 router.get('/all', product_controller.all)
 router.get('/find/:name', product_controller.find_one);
