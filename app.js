b4cffdf97335acf18780a6692b5cd8acae6e065f
James Hooper <jameshooperdesigns@gmail.com>
2019-04-29 21:09:27 -0600

    Edit app with spaces in parameter

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const product = require('./routes/product.route');
 // Initialize our express app
 const app = express();
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
